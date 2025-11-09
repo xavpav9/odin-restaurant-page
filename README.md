e976b02408d143460ffcee6b44934682363f8649
Xavier Tondelier <xtondelier@gmail.com>
2025-11-09 02:04:00 +0000

    Add home page
    Includes timetable and basic introductory texts.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Restaurant Page
 
 Another project from The Odin Project which goes into using webpack (a bundler), npm and using JavaScript to generate the website content.
+
+### Credit:
+- Opening Hours Salad Picture by [Mae Mu](https://unsplash.com/@picoftasty?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText) on [Unsplash](https://unsplash.com/photos/green-cabbage-vegetable-hugb85v0J04?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText)
+      
--- src/home.js
+++ src/home.js
@@ -7,8 +7,51 @@ export default function() {
 
   const introductoryPara = document.createElement("p");
   introductoryPara.textContent = "Welcome to the home page of the salad bar. We have many different foods for you to choose from, but mainly focused on salad. We hope that you can come to our restaurant and enjoy our exquisite salads with us.";
+  introductoryPara.classList.add("intro-para");
+
+  const openingHours = document.createElement("div");
+  const openingHoursTable = document.createElement("table");
+  const openingHoursThead = document.createElement("thead");
+  const openingHoursTbody = document.createElement("tbody");
+  const openingHoursTheadRow = document.createElement("tr");
+  ["", "Opening Times: ", "Closing Times: "].forEach(cell => {
+    const th = document.createElement("th");
+    th.textContent = cell;
+    if (cell !== "") th.scope = "col";
+    openingHoursTheadRow.appendChild(th);
+  });
+  openingHoursThead.appendChild(openingHoursTheadRow);
+  [{day: "Monday", open: "9:00", close: "18:00"},
+   {day: "Tuesday", open: "9:00", close: "18:00"},
+   {day: "Wednesday", open: "9:00", close: "18:00"},
+   {day: "Thursday", open: "9:00", close: "18:00"},
+   {day: "Friday", open: "9:00", close: "19:00"},
+   {day: "Saturday", open: "10:00", close: "20:00"},
+   {day: "Sunday", open: "10:00", close: "16:00"},
+  ].forEach(setOfData => {
+    const tr = document.createElement("tr");
+    const th = document.createElement("th");
+    const td1 = document.createElement("td");
+    const td2 = document.createElement("td");
+
+    th.textContent = setOfData.day;
+    th.scope = "row";
+    td1.textContent = setOfData.open;
+    td2.textContent = setOfData.close;
+    tr.appendChild(th);
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    openingHoursTbody.appendChild(tr);
+  });
+
+  openingHoursTable.appendChild(openingHoursThead);
+  openingHoursTable.appendChild(openingHoursTbody);
+  openingHours.appendChild(openingHoursTable);
+  openingHours.classList.add("opening-hours");
+  
 
 
   contentDiv.appendChild(header);
   contentDiv.appendChild(introductoryPara);
+  contentDiv.appendChild(openingHours);
 }
